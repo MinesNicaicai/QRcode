a2660396e0f520b25c604b142f6853cb3cb5d4d6
MinesNicaicai <yanhao.li@mines-paristch.fr>
2019-11-10 16:34:58 +0800

    done

--- main.cpp
+++ main.cpp
@@ -2,18 +2,17 @@
 
 /** @function main */
 int main( int argc, char** argv ){
+  DEBUG = false;
 
   /// Load source image and convert it to gray
   src = imread( argv[1], 1 );
 
-  /// Convert image to gray and blur it
-  cvtColor( src, src_gray, COLOR_BGR2GRAY );
-  blur( src_gray, src_gray, Size(3,3) );
+  // Convert the source image into a binary matrix
+  code_recognition(src, out_matrix);
 
-  /// Create Window
-  char* source_window = "Source";
-  thresh_callback(0, 0);
-  // cornerHarris_demo(0, 0);
+  // Output the results to a file
+  string dst = "out.txt";
+  output_result(dst);
 
   waitKey(0);
   return(0);
@@ -23,12 +22,16 @@ int main( int argc, char** argv ){
 
 
 /** @function thresh_callback */
-void thresh_callback(int, void* ){
+void code_recognition(Mat src, unsigned int out_matrix[QRsize][QRsize]){
   
   Mat canny_output;
   Mat bin_output;
   vector< vector<Point> > contours;
   vector<Vec4i> hierarchy;
+
+
+  cvtColor( src, src_gray, COLOR_BGR2GRAY );
+  blur( src_gray, src_gray, Size(3,3) );
   
   double thres = threshold(src_gray, bin_output, 0, 255, THRESH_OTSU);
   /// Detect edges using canny
@@ -73,13 +76,6 @@ void thresh_callback(int, void* ){
   }
 
 
-  /// Show in a window
-
-
-  namedWindow( "Binary", WINDOW_AUTOSIZE );
-  imshow( "Binary", bin_output );
-  cout << " Hola !" << endl;
-
   /// Corner detectors
 
 
@@ -87,39 +83,61 @@ void thresh_callback(int, void* ){
     circle( src, corner_pts.at(i), 1,  Scalar(200), 2, 8, 0 );
   }
 
+  // Sort the 12 points to be in order
   resort_corners(corner_pts);
 
-  vector<Point> img_pts;
-  set_image_points(img_pts);
-  cout << endl << "img_pts:" ;
-  for (int i = 0; i < img_pts.size(); i++){
-    cout << img_pts[i];
-  }
-  cout << endl;
 
-  // Convert Point2i to Point2f
+
+  // Convert the corner points from Point2i to Point2f
   vector<Point2f> corner_pts_f;
-  vector<Point2f> img_pts_f;
   for(int i = 0; i < corner_pts.size(); i++){
     corner_pts_f.push_back(Point2f(corner_pts[i].x, corner_pts[i].y));
   }
-  for(int i = 0; i < img_pts.size(); i++){
-    img_pts_f.push_back(Point2f(img_pts[i].x, img_pts[i].y));
+
+  // Set the destination points after homography transformation  
+  vector<Point2f> img_pts_f;
+  set_dst_points(img_pts_f);
+
+  // Do the homography transformation
+  Mat Hr = findHomography(corner_pts_f, img_pts_f, 0, 3);
+  Mat dst;
+  warpPerspective(bin_output, dst, Hr, src.size(), INTER_LINEAR, BORDER_CONSTANT, Scalar());
+
+  // Transform the regulated binary image to a 42x42 binary matrix 
+  transformToMatrix(dst, out_matrix);
+  
+
+  if (DEBUG){
+    cout << "M = "<< endl << " "  << Hr << endl << endl;
+    namedWindow( "Binary QR code", WINDOW_AUTOSIZE );
+    imshow( "Binary QR code", bin_output );
+    namedWindow( "Transformed QR code", WINDOW_AUTOSIZE );
+    imshow( "Transformed QR code", dst );
   }
 
-  Mat Hr = findHomography(corner_pts_f, img_pts_f, RANSAC, 3);
-  Mat dst;
-  warpPerspective(src, dst, Hr, src.size(), INTER_LINEAR, BORDER_CONSTANT, Scalar());
 
 
-  cout << "M = "<< endl << " "  << Hr << endl << endl;
+
+  if (DEBUG){
+    for (int i = 0; i <= QRsize; i++){
+      line(dst, Point(origin_x + i * grid_width, 0), Point(origin_x + i * grid_width, 1000), Scalar(127));
+      line(dst, Point(0, origin_y + i * grid_height), Point(1000, origin_y + i * grid_height), Scalar(127));
+
+    }
+    namedWindow( "draw lines", WINDOW_AUTOSIZE);
+    imshow("draw lines", dst);
+  }
 
 
-  namedWindow( "corners_window", WINDOW_AUTOSIZE );
-  imshow( "corners_window", src_corners );
-
-  namedWindow( "warpPerspective", WINDOW_AUTOSIZE );
-  imshow( "warpPerspective", dst );
+  if (DEBUG){
+    for (int j = 0; j < QRsize; j++){
+      for (int i = 0; i < QRsize; i++){
+        cout << out_matrix[i][j] << ", ";
+      }
+      cout << endl;
+    }
+  }
+  
 
 }
 
@@ -130,15 +148,16 @@ void resort_corners(vector<Point> &points){
    * input: points = 12 x Point, 0-3, 4-7, 8-11 are three groups of points 
    * belonging to three corners.
    * First we reorder the three corners as follows:
-   *  c0------c2       -----> x
-   *  |      /         |   
-   *  |     /          | 
-   *  |    /           v  y
-   *  |   /
+   *  c0-----------c2       -----> x
+   *  |          /          |   
+   *  |        /            | 
+   *  |      /              v  y
+   *  |    /
    *  |  /
    *  c1
+   * 
    * Then we reorder the 4 points of each corner
-   *  p0----p1
+   *  p0----p1 
    *  |     |
    *  |     |
    *  p2----p3
@@ -220,7 +239,8 @@ void sort_corners(vector<Point> &points){
 }
 
 void sort_corner0(vector<Point> &points, int* pos){
-  /**  p0----p1
+  /**  Sort the 4 points of corner0 (the top left corner)
+   *   p0----p1
    *   |     |
    *   |     |
    *   p2----p3
@@ -252,7 +272,8 @@ void sort_corner0(vector<Point> &points, int* pos){
 
 void sort_corner1(vector<Point> &points, int* pos){
 
-  /**  p4----p5
+  /**  Sort the 4 points of corner1 (the bottom left corner)
+   *   p4----p5
    *   |     |
    *   |     |
    *   p6----p7
@@ -267,7 +288,6 @@ void sort_corner1(vector<Point> &points, int* pos){
   for (int i = 0; i < 4; i++){
     dist2c.push_back(get_distance(points[i + 4], c0));
   }
-  cout << "Hola ! " << endl;
   sort_indexes(dist2c, pos_local);
   for (int i = 0; i < 4; i++){
     pos[pos_local[i] + 4] = i < 2 ? 4 : 7;
@@ -286,7 +306,8 @@ void sort_corner1(vector<Point> &points, int* pos){
 
 void sort_corner2(vector<Point> &points, int* pos){
 
-  /**  p8-----p9
+  /**  Sort the 4 points of corner2 (the top right corner)
+   *   p8-----p9
    *   |      |
    *   |      |
    *   p10----p11
@@ -317,27 +338,52 @@ void sort_corner2(vector<Point> &points, int* pos){
 
 }
 
-void set_image_points(vector<Point> &img_pts){
-  grid_width = 80;
-  vector<Point> square;
-  square.push_back(Point(0, 0));
-  square.push_back(Point(grid_width, 0));
-  square.push_back(Point(0, grid_height));
-  square.push_back(Point(grid_width, grid_height));
+void set_dst_points(vector<Point2f> &img_pts){
+
+  vector<Point2f> square;
+  square.push_back(Point2f(0, 0));
+  square.push_back(Point2f(6 * grid_width, 0));
+  square.push_back(Point2f(0, 6 * grid_height));
+  square.push_back(Point2f(6 * grid_width, 6 * grid_height));
 
   img_pts.clear();
   for (int j = 0; j < 4; j++){
-    img_pts.push_back(square[j] + Point(300, 300));
+    img_pts.push_back(square[j] + Point2f(origin_x, origin_y));
   }
   for (int j = 0; j < 4; j++){
-    img_pts.push_back(square[j] + Point(300, 300 + 6 * grid_height));
+    img_pts.push_back(square[j] + Point2f(origin_x, origin_y + 36 * grid_height));
   }
   for (int j = 0; j < 4; j++){
-    img_pts.push_back(square[j] + Point(300 + 6 * grid_width, 300));
+    img_pts.push_back(square[j] + Point2f(origin_x + 36 * grid_width, origin_y));
   }
   
 }
 
-Mat get_Hr(const vector<Point> &corner_pts, const vector<Point> &img_pts){
-  return Mat::zeros(src.size(), CV_8UC1);
+void transformToMatrix(Mat in_bin_img, unsigned int out_matrix[QRsize][QRsize]){
+  Mat tmp;
+  for (int i = 0; i < QRsize; i++){
+    for (int j = 0; j < QRsize; j++){
+      tmp = in_bin_img(Rect(origin_x + i * grid_width, origin_y + j * grid_height, 
+                              grid_width, grid_height));
+
+      double m = mean(tmp).val[0];
+      out_matrix[i][j] = m > 128 ? 1 : 0;
+    }
+  }
+}  
+
+void output_result(string dst){
+  /**
+   * Output the matrix to a file
+   */
+  ofstream dst_file;
+  dst_file.open (dst);
+  dst_file << "Output matrix: \n\n";
+  for (int j = 0; j < QRsize; j++){
+    for (int i = 0; i < QRsize; i++){
+      dst_file << out_matrix[i][j] << ", ";
+    }
+    dst_file << endl;
+  }
+  dst_file.close();
 }
--- main.hpp
+++ main.hpp
@@ -2,6 +2,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/calib3d/calib3d.hpp>
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -11,19 +12,30 @@
 using namespace cv;
 using namespace std;
 
-Mat src; Mat src_gray;
+// Input
+Mat src;
+
+// Output 
+const int QRsize = 42;
+unsigned int out_matrix[QRsize][QRsize];
+
+// Other global variables
+Mat src_gray;
 int thresh = 100;
 int max_thresh = 255;
-int grid_width = 80;
-int grid_height = 80;
+int grid_width = 14;
+int grid_height = 14;
+int origin_x = 300;
+int origin_y = 300;
 RNG rng(12345);
 
 Mat src_corners;
 
+bool DEBUG = true;
 
 
-/// Function header
-void thresh_callback(int, void* );
+/// Function headers
+void code_recognition(Mat, unsigned int[QRsize][QRsize]);
 
 void resort_corners(vector<Point> &, vector<int> & );
 
@@ -47,6 +59,8 @@ void sort_corner1(vector<Point> &, int*);
 
 void sort_corner2(vector<Point> &, int*);
 
-void set_image_points(vector<Point> &img_pts);
+void set_dst_points(vector<Point2f> &img_pts);
 
-Mat get_Hr(const vector<Point> &corner_pts, const vector<Point> &img_pts);
+void transformToMatrix(Mat, unsigned int[QRsize][QRsize]);
+
+void output_result(string);
